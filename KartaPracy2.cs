4e5523a3f12ca9f8bb1d287f6e6ac99b788d8767
Maksim Fedoseev <114389643+infstudy@users.noreply.github.com>
2022-10-10 10:31:54 +0200

    Create KartaPracy2.cs

---
+++ KartaPracy2.cs
@@ -0,0 +1,92 @@
+// zad 1
+/*int a = int.Parse(Console.ReadLine());
+if (a % 3 == 0)
+{
+    Console.WriteLine("TAK");
+}
+else
+{
+    Console.WriteLine("NIE");
+}
+*/
+
+// zad 2
+/*int a = int.Parse(Console.ReadLine());
+if (a >= 100 && a < 1000 && a % 17 == 0)
+{
+    Console.WriteLine("TAK");
+}
+else
+{
+    Console.WriteLine("NIE");
+}
+*/
+
+//zad 3
+/*int wiek = int.Parse(Console.ReadLine());
+*//*if wiek >= 18)
+{
+    Console.WriteLine("Jest Pełnoletnią");
+}
+else
+{
+    Console.WriteLine("Nie jest");
+}*//*
+Console.WriteLine(wiek >= 18 ? "TAK" : "NIE");*/
+
+// zad 4
+/*
+int waga = int.Parse(Console.ReadLine());
+const int limit = 20;
+if (waga > limit)
+{
+    Console.WriteLine("nie");
+}
+else
+{
+    Console.WriteLine("tak");
+}
+*/
+
+// zad 5
+/*
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+if ((c > a && c < b) || (c > b && c < a))
+{
+    Console.WriteLine("Tak");
+}
+else
+{
+    Console.WriteLine("NIE");
+}
+*/
+
+// zad 6
+/*
+int p = int.Parse(Console.ReadLine());
+int a = int.Parse(Console.ReadLine());
+if (((Math.Pow(a, p)) - a) % p == 0)
+{
+    Console.WriteLine("Tak");
+}
+else
+{
+    Console.WriteLine("NIE");
+}
+*/
+
+// zad 7
+/*
+int p = int.Parse(Console.ReadLine());
+int k = int.Parse(Console.ReadLine());
+int s = int.Parse(Console.ReadLine());
+if ((p >= 0) && (k >= 0) && (s >= 0) && (s >= (k - p)))
+{
+    Console.WriteLine("TAK");
+}
+else
+{
+    Console.WriteLine("NIE");
+}*/
