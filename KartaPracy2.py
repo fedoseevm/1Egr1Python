378bf7e07492457d2200dfdb1d8f6d3bc7023df8
Maksim Fedoseev <114389643+infstudy@users.noreply.github.com>
2022-10-10 14:39:17 +0200

    Create KartaPracy2.py

---
+++ KartaPracy2.py
@@ -0,0 +1,43 @@
+# zadanie 1
+# n = int(input())
+# if n % 3 == 0:
+#     print('jest poszielna')
+# else:
+#     print('Nie jest')
+
+# zadanie 2
+# a = int(input())
+# if (a>=100) and (a<1000) and (a % 17 == 0):
+#     print('tak')
+# else:
+#     print('nie')
+
+# zad 3
+# a = int(input())
+# if a >= 18:
+#     print('osoba jest pełnoletnia')
+# else:
+#     print('nie jest')
+
+# zad 4
+# LIMIT = 20
+# waga = int(input())
+# if  waga < LIMIT:
+#     print('Mozna wjechac')
+# else:
+#     print('nie mozna wjechac')
+
+# zad 5
+a = int(input())
+b = int(input())
+c = int(input())
+if a > c:
+    if b < c:
+        print('tak')
+    else:
+        print('nie')
+else:
+    if b > c:
+        print('tak')
+    else:
+        print('nie')
