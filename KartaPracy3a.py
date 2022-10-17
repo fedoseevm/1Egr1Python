f04feeff04d2ee7b7f7dcb8327563fd4b9af97b1
Maksim Fedoseev <114389643+infstudy@users.noreply.github.com>
2022-10-17 15:09:35 +0200

    Create KartaPracy3a.py

---
+++ KartaPracy3a.py
@@ -0,0 +1,30 @@
+# for i in range(1, 11):
+#     for j in range(1, 11):
+#         print(i*j, end="\t")
+#     print()
+
+# *
+# **
+# ***
+# ****
+
+n = int(input())
+for i in range(n):
+    for j in range(i+1):
+        print("*", end="")
+    print()
+
+print()
+print()
+
+for i in range(n):
+    for j in range(n-i):
+        print("*", end="")
+    print()
+
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ", end="")
+    for k in range(n-i-1, n):
+        print("*", end="")
+    print()
