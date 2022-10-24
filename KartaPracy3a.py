4784da44237d40bb6c98d0bb6e2cb93e802a057f
Maksim Fedoseev <114389643+infstudy@users.noreply.github.com>
2022-10-24 15:10:02 +0200

    Update KartaPracy3a.py

--- KartaPracy3a.py
+++ KartaPracy3a.py
@@ -44,3 +44,53 @@ for i in range(n):
     else:
         print("--", end="")
 print()
+
+# zad 3
+#n = int(input())
+#for i in range(1,n+1):
+#    print("*", end="")
+#    if i%2==1:
+#        print("|"*i, end="")
+#    else:
+#        print("-"*i, end="")
+
+# pre
+#n = int(input())
+#for i in range(n):
+#    for j in range(n):
+#        print("*", end="")
+#    print()
+
+
+# zad 5
+n = int(input())
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if j==n//2+1:
+            print("*", end="")
+        elif i==n//2+1:
+            print("-", end="")
+        else:
+            print(" ", end="")
+    print()
+
+    if 5:
+        if 6:
+            if 6:
+                pass
+            else:
+                pass
+        else:
+            if 6:
+                pass
+
+
+    # zad 7
+#n = int(input())
+#for i in range(1,n+1):
+#    for j in range(1,n+1):
+#        if i == 1 or i == n or j == 1 or j == n or (i==n//2+1 and j ==n//2+1):
+#            print("*", end="")
+#        else:
+#            print(" ", end="")
+#    print()
