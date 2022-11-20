3db9dd205546b53517428603f2746db0c8e34980
Maksim Fedoseev <114389643+infstudy@users.noreply.github.com>
2022-11-20 20:03:42 +0100

    Update Pierwsze.py

--- Pierwsze.py
+++ Pierwsze.py
@@ -1,19 +1,26 @@
-# 1. Algorytm sprawdzający czy liczba jest pierwsza
-# liczba pierwsza dzieli sie tylko przez 1 i samą siebie
+# 1. Algorytm sprawdzajacy czy liczba jest pierwsza
+# liczba pierwsza dzieli sie tylko przez 1 i sama siebie
 # 2, 3, 5, 7, 11, 13, 17, 19 itd...
-# liczba x ma swój dzielnik (o ile go ma) w przedziale [2, sqrt(x)]
+# liczba x ma swoj dzielnik (o ile go ma) w przedziale [2, sqrt(x)]
 # dzielnik wlasciwy - dzielnik liczby oprocz 1 i niej samej
+
 #n = int(input())
 #for i in range(2,int(n**0.5)+1):
 #    if n % i == 0:
-#        print("liczba nie jest pierwsza")
-#        exit(0)
-#print("liczba jest pierwsza")
-
+#        print("Liczba nie jest pierwsza")
+#        exit()
+#print("Liczba jest pierwsza")
 
 # 2. Generator liczb pierwszych - liczby pierwsze w przedziale [p, q]
 
-# [10, 15] - 11 i 13
-# [20, 22] - brak liczb pierwszych
+# [10;15] -> 11 i 13
+# [20;22] -> brak liczb pierwszych
 
-# 3. Generator liczb pierwszych - początkowe k liczb pierwszych
+p, q = map(int, input().split())
+for i in range(p, q+1):
+    for j in range(2,int(i**0.5)+1):
+        if i % j == 0:
+            break
+    else:
+        print(i, end=" ")
+# 3. Generator liczb pierwszych - poczatkowe k liczb pierwszych
