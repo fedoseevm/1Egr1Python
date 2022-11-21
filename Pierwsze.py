f85a0168809eea0dc2a64328e4882ada4f5773b2
Maksim Fedoseev <114389643+infstudy@users.noreply.github.com>
2022-11-21 15:09:26 +0100

    Update Pierwsze.py

--- Pierwsze.py
+++ Pierwsze.py
@@ -7,20 +7,70 @@
 #n = int(input())
 #for i in range(2,int(n**0.5)+1):
 #    if n % i == 0:
-#        print("Liczba nie jest pierwsza")
-#        exit()
-#print("Liczba jest pierwsza")
+#        print("liczba nie jest pierwsza")
+#        exit(0)
+#print("liczba jest pierwsza")
+
 
 # 2. Generator liczb pierwszych - liczby pierwsze w przedziale [p, q]
 
-# [10;15] -> 11 i 13
-# [20;22] -> brak liczb pierwszych
+# [10, 15] - 11 i 13
+# [20, 22] - brak liczb pierwszych
 
-p, q = map(int, input().split())
-for i in range(p, q+1):
-    for j in range(2,int(i**0.5)+1):
-        if i % j == 0:
-            break
-    else:
-        print(i, end=" ")
-# 3. Generator liczb pierwszych - poczatkowe k liczb pierwszych
+#l = "angole to wory".split()
+#print(l)
+#l = list(map(int, "880-055-535-35".split("-")))
+#print(l)
+
+#p, q = map(int, input().split())
+#for i in range(p,q+1):
+#    flaga = True
+#    for j in range(2,int(i**0.5)+1):
+#        if i%j==0:
+#            flaga = False
+#            break
+#    if flaga:
+#        print(i, end=" ")
+
+#3 Generator liczb pierwszych - poczatkowe k liczb pierwszych
+
+#n = int(input())
+#x = 2
+#while 1:
+#    flaga = True
+#    for i in range(2, x):
+#        if x % i == 0:
+#            flaga = False
+#            break
+
+#    if flaga:
+#        print(x, end=" ")
+#        n = n - 1
+#        if n == 0:
+#            break
+#    x += 1
+
+# inny sposob (zmiana w 'while' i brak ostatniego 'if')
+#n = int(input())
+#x = 2
+#while n > 0:
+#    flaga = True
+#    for i in range(2, x):
+#        if x % i == 0:
+#            flaga = False
+#            break
+
+#    if flaga:
+#        print(x, end=" ")
+#        n = n - 1
+#    x += 1
+
+# NWD liczb (Najwiekszy wspolny dzielnik)
+a = int(input())
+b = int(input())
+while a != b:
+    if a > b:
+        a -= b
+    if b > a:
+        b -= a
+print(a)
