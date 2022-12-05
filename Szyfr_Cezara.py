f8daee7c9972a21ebd424e106f7f849afc1a3258
Maksim Fedoseev <114389643+infstudy@users.noreply.github.com>
2022-12-05 15:09:46 +0100

    Create Szyfr_Cezara.py

---
+++ Szyfr_Cezara.py
@@ -0,0 +1,43 @@
+# Szyfr Cezara
+
+# Funkcja ord() - zwraca kod ASCII podasnego znaku
+#print(ord("A"))
+#print(ord("Z"))
+
+# Funkcja chr() - zwraca znak dla podanego kodu ASCII
+#print(chr(65))
+#print(chr(77))
+
+# Polanczenie w calosc:
+#print(chr(ord("C")+2))
+
+# Alfabet A - Z za pomoca FOR:
+for i in range(65, 91):
+    print(chr(i), end=" ")
+print()
+
+# Jak z napisu wyluskac litery
+#napis = "POLSKA"
+#print(len(napis))
+#print(napis[1])
+
+# Wypisz litery napisu w petli:
+#napis = input()
+#for i in range(len(napis)):
+#    print(napis[i], end=" ")
+
+# Wypisz kody ASCII liter
+#napis = input()
+#for i in range(len(napis)):
+#    print(ord(napis[i]), end=" ")
+
+# SZYFR CEZARA prosty
+napis = input()
+for i in range(len(napis)):
+    print(chr(ord(napis[i]) + 3), end=" ")
+
+# ZLOZONY
+napis = input()
+szyfr = ""
+for i in range(len(napis)):
+    szyfr = szyfr + chr(65 + (ord(napis[i] - 65 + 3) % 26))
