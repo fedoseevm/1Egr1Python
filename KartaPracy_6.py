74a20b6d37b84a63b0e22a974bf3b2381f2e6598
Maksim Fedoseev <114389643+infstudy@users.noreply.github.com>
2022-12-16 21:07:49 +0100

    Dodanie zadan 5-7

--- KartaPracy_6.py
+++ KartaPracy_6.py
@@ -34,14 +34,37 @@
 #        ilosc += 1
 #print(ilosc)
 
+# zad 5
+#ilosc = 0
+#suma = 0
+#for i in range(100, 1000):
+#    if (i % 100) * 100 > (i // 10) % 10 + (i % 10):
+#        ilosc += 1
+#        suma += i
+#print(f"Ilosc: {ilosc}")
+#print(f"Suma: {suma}")
+
 # zad 6
 #n = int(input())
 #suma = 0
 #i = 1
 #while n > 0:
-
+#    if i % 19 == 0:
+#        suma += i
+#        n -= 1
+#    i = i + 1
+#print(suma)
 
 # zad 7
+#n = int(input())
+#suma = 0
+#i = 999
+#while n > 0:
+#    if i % 37 == 0:
+#        suma += i
+#        n -= 1
+#    i -= 1
+#print(suma)
 
 # zad 8
 n = int(input())
