f643bf32a7d54688baa63f5cfbd7fe336c3862bd
Maksim Fedoseev <114389643+infstudy@users.noreply.github.com>
2022-12-25 15:38:02 +0100

    Zrobione

--- Dodatkowe.py
+++ Dodatkowe.py
@@ -1,14 +1,19 @@
-n = int(input())
-liczba1 = 0
-liczba2 = 0
-czy_pierwsza = True
-for i in range(3, n + 1):
-    for j in range(2, int(n ** 0.5) + 1):
-        if i % j == 0:
-            czy_pierwsza = False
-            break
-    if czy_pierwsza:
-        i = liczba1
-        exit()
-liczba2 = n - liczba1
-print(f"liczba {n} skada sie z liczb {liczba1} i {liczba2}")
+def czy_pierwsza(x):
+    czy_p = True
+    for i in range(2, int(x ** 0.5) + 1):
+        if x % i == 0:
+            czy_p = False
+    if czy_p:
+        return True
+    else:
+        return False
+x = int(input())
+czy = False
+for j in range(2, x // 2 + 1):
+    if czy_pierwsza(j) and czy_pierwsza(x - j):
+        czy = True
+        print(j, x - j)
+if czy:
+    print("Tak")
+else:
+    print("Nie")
