eda6752da856ac66b1ebafbd5421fe99f913512e
Maksim Fedoseev <114389643+infstudy@users.noreply.github.com>
2023-01-25 15:11:00 +0100

    Update szyfr_RSA.py

--- szyfr_RSA.py
+++ szyfr_RSA.py
@@ -1,29 +1,45 @@
-# Algorytm RSA
-# szyfr RSA
-#from hashlib import md5, sha256
-#user = kamilek
-#pass = nigeria123
-#print(md5(b"lorem").hexdigest())
-#print(md5(b"Iorem").hexdigest())
-#print(md5(b"Lorem").hexdigest())
+#   RSA
+from math import gcd    # gcd -- NWD dwuch liczb
+print(gcd(20, 15))      # wyjscie : 5
 
-#x*a + y*b  = NWD(a, b)
-#x*7 + y*11 = NWD(7,11) = 1
-#8*7 - 5*11
+# 1. Wybor duzych liczb pierwszych
+p = 11
+q = 13
+print(p,q)
 
-# biore 2 duze liczby pierwsze
-p = 13
-q = 11
-
-# szukam f = (p - 1) * (q - 1) tzw Funkcja Eulera
-F = (p - 1) * (q - 1)
-
-# Obliczam modul n = p * q
+# 2. Obliczyc n = p * q oraz funkcje Eulera F = (p - 1)(q - 1)
 n = p * q
+F = (p - 1)* (q - 1)
+print(n)
+print(F)
 
-# wyznaczamy klucz publiczny e, ma on byc wzglednie perwszy z F
-# algorytm Euklidesa...
-# NWD(e, F) = 1
-#np NWD(143, 7) = 1
-# wyznaczamy klucz prywatny d, ma on byc wzglednie perwszy z n
-NWD(d, n = 1)
+# 3. Generowanie klucza publicznego e : NWD(F, e) = 1
+for i in range(2, F):
+    if gcd(i,F) == 1:
+        e = i
+        break
+print(e, n)
+
+# 4. Generowanie klucza prywatnego d: (e * d) % F = 1 (tzw. odrwotnosc modulo)
+for j in range(2, F):
+    if e * j % F == 1:
+        d = j
+        break
+print(d, n)
+
+
+# Przyklad dzialania
+# m - message (wiadomosc dla zaszyfrowania)
+# c = m ** e % n (cipher - szyfrogram - tekst szyfrogramy)
+# t = c ** d % n (tekst jawny, czyli wiadomosc n)
+
+m = input()
+cipher = ""
+for i in m:
+    cipher += chr((ord(i) ** e) % n)
+print(cipher)
+
+tekst = ""
+for j in cipher:
+    tekst += chr((ord(j) ** d) % n)
+print(tekst)
