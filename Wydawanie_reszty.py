3aa40f2b6658a1d0782c4fa6933b97bcc8cb12f4
Maksim Fedoseev <114389643+infstudy@users.noreply.github.com>
2023-04-11 16:23:21 +0200

    Update Wydawanie_reszty.py

--- Wydawanie_reszty.py
+++ Wydawanie_reszty.py
@@ -1,13 +1,32 @@
-# wydawanie reszty
-x = int(input())
-N = [200, 100, 50, 20, 10, 5, 2, 1]
-reszta = []
-for i in N:
-    ilosc = x // i
-    if ilosc > 0:
-        x = x - ilosc * i
-        #print(f"{i}zl razy {ilosc}")
-        for j in range(ilosc):
-            reszta.append(i)
-for i in reszta:
-    print(i)
+# ALGORYTM WYDAWANIA RESZTY
+N = [500, 200, 100, 50, 20, 10, 5, 2, 1]
+kwota = int(input())
+while kwota > 0:
+    for nominal in N:
+        ilosc = kwota // nominal
+        kwota -= nominal * ilosc
+        if ilosc > 0:
+            print(f"{nominal} -- {ilosc}")
+            print()
+
+# FUNKCJA WYDAJACA RESZTE
+def reszta(kwota):
+    N = [500, 200, 100, 50, 20, 10, 5, 2, 1]
+    reszta = []
+    for nominal in N:
+        while kwota > nominal:
+            kwota -= nominal
+            reszta.append(nominal)
+    return reszta
+
+# FUNKCJA WYPISUJACA NOMINALY Z ILOSCIA
+def reszta_il(kwota):
+    nominaly = [500, 200, 100, 50, 20, 10, 5, 2, 1]
+    reszta = []
+    while kwota > 0:
+        for nominal in nominaly:
+            ilosc = kwota // nominal
+            if ilosc > 0:
+                kwota -= nominal * ilosc
+                reszta.append(f"{nominal} -- {ilosc}")
+    return reszta
