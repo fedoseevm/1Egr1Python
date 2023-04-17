e72fdcf6573101e6fa7bc3f4ddb75ded5f0c492f
Maksim Fedoseev <114389643+infstudy@users.noreply.github.com>
2023-04-17 15:00:34 +0200

    Dodanie zadań drugiej grupy

--- kartkowka_17_04.py
+++ kartkowka_17_04.py
@@ -13,3 +13,17 @@ for i in range(len(napis) - 1):
     if napis[i] == napis[i + 1]:
         k += 1
 print(f"Ilosc sytuacji, kiedy wystepuja kolo siebie dwie takie same literki: {k}")
+
+# Zadanie 3(1)
+wyraz = input()
+print(len(set(wyraz)))
+
+# Zadanie 4(2)
+slowo2 = input()
+wynik = ""
+print("\n")
+for i in range(len(slowo2) // 2):
+    wynik += slowo2[i] + slowo2[-i - 1]
+if len(slowo2) % 2:
+    wynik += slowo2[len(slowo2)//2]
+print(wynik)
